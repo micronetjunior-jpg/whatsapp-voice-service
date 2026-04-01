c04229b8dac2651624f906263bddfbaab284faa4
Domentia <micronetjunior@icloud.com>
2026-04-01 07:06:44 -0500

    ok

--- index.js
+++ index.js
@@ -358,10 +358,11 @@ async function crearPeer(callId) {
   pc.onicecandidate = (event) => {
     if (!event.candidate) {
       console.log(`[${callId}] ICE gathering finalizado`);
-      return;
+      //return;
+    }else
+    {
+      console.log(`[${callId}] ICE local: ${event.candidate.candidate}`);
     }
-    console.log(`[${callId}] ICE local: ${event.candidate.candidate}`);
-  
   };
 
   pc.oniceconnectionstatechange = () => {
